d9d20434a09f7855184e9860c2db495bbfd9d874
jgoodson2 <jonathongoodson@clayton.edu>
2015-11-18 16:38:38 -0500

    Create controller convert.

---
+++ grails-app/controllers/converter/ConvertController.groovy
@@ -0,0 +1,6 @@
+package converter
+
+class ConvertController {
+
+    def index() { }
+}
---
+++ test/unit/converter/ConvertControllerSpec.groovy
@@ -0,0 +1,20 @@
+package converter
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(ConvertController)
+class ConvertControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
